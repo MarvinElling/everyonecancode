ba6f0a8fa93da9351c189a58bab4d96b5cb797b8
Marvin Elling <marvin.elling@gmail.com>
2024-05-13 13:56:02 +0000

    Sprint 6

--- recommendation_engine.py
+++ recommendation_engine.py
@@ -5,10 +5,7 @@ from services import Service
 from openai import AzureOpenAI
 from dotenv import dotenv_values
 
-
-
 class RecommendationEngine:
-    
     def __init__(self):
         config = dotenv_values(".env")
 
@@ -27,21 +24,24 @@ class RecommendationEngine:
             raise Exception("OpenAI not implemented")  
 
 
-    async def get_recommendations(self, keyword_phrase):
+    async def get_recommendations(self, keyword_phrase, previous_links_str=None):
         prompt = f"""Please return 5 recommendations based on the input string: '{keyword_phrase}' using correct JSON syntax that contains a title and a hyperlink back to the supporting website. RETURN ONLY JSON AND NOTHING ELSE"""
         system_prompt = """You are an administrative assistant bot who is good at giving 
-        recommendations for tasks that need to be done by referencing website links that can provide 
-        assistance to helping complete the task. 
+            recommendations for tasks that need to be done by referencing website links that can provide 
+            assistance to helping complete the task. 
 
-        If there are not any recommendations simply return an empty collection. 
+            If there are not any recommendations simply return an empty collection. 
 
-        EXPECTED OUTPUT:
-        Provide your response as a JSON object with the following schema:
-        [{"title": "...", "link": "..."},
-        {"title": "...", "link": "..."},
-        {"title": "...", "link": "..."}]
-        """
-        
+            EXPECTED OUTPUT:
+            Provide your response as a JSON object with the following schema:
+            [{"title": "...", "link": "..."},
+            {"title": "...", "link": "..."},
+            {"title": "...", "link": "..."}]
+            """
+            
+        if previous_links_str is not None:
+            prompt = prompt + f". EXCLUDE the following links from your recommendations: {previous_links_str}"  
+
         message_text = [{"role":"system","content":system_prompt},
                         {"role":"user","content":prompt},]
 
@@ -69,7 +69,7 @@ class RecommendationEngine:
 
 async def test_recommendation_engine():
     engine = RecommendationEngine()
-    recommendations = await engine.get_recommendations("Rent bike munich")
+    recommendations = await engine.get_recommendations("Buy a birthday gift for mom")
     count = 1
     for recommendation in recommendations:
         print(f"{count} - {recommendation['title']}: {recommendation['link']}")
@@ -77,4 +77,3 @@ async def test_recommendation_engine():
 
 if __name__ == "__main__":
     asyncio.run(test_recommendation_engine())
-    
